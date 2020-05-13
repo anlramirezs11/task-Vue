9a0e13fe32b93058703f9c6676c6979fdc3e4190
Andrea Ramirez <anlramirezs11@gmail.com>
2020-05-12 20:11:15 -0500

    Se agrega la tabla para manejo de tareas y se crea el v-for para recorrer el arreglo de tareas

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
+var tareas = [
+    {
+        title: "Task 1",
+        done: true
+    },
+    {
+        title: "Task 2",
+        done: true
+    },
+    {
+        title: "Task 3",
+        done: true
+    },
+    {
+        title: "Task 4",
+        done: true
+    },
+    {
+        title: "Task 5",
+        done: false
+    }
+]
+
 new Vue({
-    el:'#task-app',
-    data:{
-        nameApp: 'Tareas Vue'
+    el: '#task-app',
+    data: {
+        nameApp: 'Tareas Vue',
+        tareas: tareas
+    },
+    methods: {
+        deleteTask: () => {
+
+        }
     }
 })
